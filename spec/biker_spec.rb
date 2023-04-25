9e0dac126f079b947eb9cc64eadf04784891d37f
Nick Tassinari <ntass7800@gmail.com>
2023-04-25 09:51:50 -0600

    finish iteration 2 tests

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -22,4 +22,44 @@ RSpec.describe Biker do
       expect(biker.acceptable_terrain).to eq([:gravel, :hills])
     end
   end
+
+  describe '#log_ride' do 
+    it 'can keep track of rides and personal record' do 
+      biker = Biker.new("Kenny", 30)
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain!(:hills)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+      expected = {
+        ride1 => [92.5, 91.1],
+        ride2 => [60.9, 61.6]
+      }
+      expect(biker.rides).to eq(expected)
+      expect(biker.personal_record(ride1)).to eq(91.1)
+      expect(biker.personal_record(ride2)).to eq(60.9)
+    end
+
+    it 'can add second biker and verify if terrain has been learned' do 
+      biker2 = Biker.new("Athena", 15)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker2.log_ride(ride1, 97.0)
+      biker2.log_ride(ride2, 67.0)
+
+      expect(biker2.rides).to eq({})
+
+      biker2.learn_terrain!(:gravel)
+      biker2.learn_terrain!(:hills)
+      biker2.log_ride(ride1, 95.0)
+      biker2.log_ride(ride2, 65.0)
+
+      expect(biker2.rides).to eq(ride2 => [65.0])
+      expect(biker2.personal_record(ride2)).to eq(65.0)
+      expect(biker2.personal_record(ride1)).to eq(false)
+    end
+  end
 end
