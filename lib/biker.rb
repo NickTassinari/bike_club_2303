bb6553b4be597e9aa591d3e9d4af5716085bb9cb
Nick Tassinari <ntass7800@gmail.com>
2023-04-25 09:51:35 -0600

    finish iteration 2

--- lib/biker.rb
+++ lib/biker.rb
@@ -12,4 +12,24 @@ class Biker
     acceptable_terrain << terrain
   end
 
+  def log_ride(ride, personal_rec)
+    if acceptable?(ride)
+      if rides[ride].nil?
+        rides[ride] = [personal_rec]
+      else  
+        rides[ride] << personal_rec
+      end
+      rides 
+    end 
+  end 
+
+  def personal_record(ride)
+    return false if rides[ride].nil? 
+      rides[ride].min
+  end
+    
+  def acceptable?(ride)
+    return true if ride.total_distance < @max_distance && acceptable_terrain.include?(ride.terrain)
+  end
+
 end
